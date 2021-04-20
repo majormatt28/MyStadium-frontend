0f749fc5a414d3b45afc37f21104b5cd87d18ed7
github username <github email address>
2021-04-20 18:54:53 -0400

    working on clickevents

--- src/index.js
+++ src/index.js
@@ -1,31 +1,94 @@
-const allCards = document.querySelectorAll('.card')
 
-function renderOneCard(stadiumObj) {
-    const outerDiv = document.createElement('div')
-    outerDiv.classList.add('card')
-    outerDiv.dataset.id = stadiumObj.id
+const stadiumCollection = document.querySelector("#stadium-collection")
 
-    outerDiv.innerHTML = `<div class="img-container">
-        <img src="${stadiumObj.image}"
-            alt="${stadiumObj.name}" />
-        <div class="stadium-description-container">
-            <h4>${stadiumObj.description}</h4>
-        </div>
-    </div>
+function renderOneCard (stadiumObj) {
    
-    `
+    const stadiumDiv = document.createElement('div')
+    stadiumDiv.classList.add('card')
+    stadiumDiv.dataset.id = stadiumObj.id
 
-    const collectionDiv = document.querySelector('div#collection')
-    collectionDiv.append(outerDiv)
+    console.log(stadiumObj)
+    stadiumDiv.innerHTML = 
+       ` <img src="${stadiumObj.image}" 
+            alt="${stadiumObj.name}" />
+      
+            <h2>${stadiumObj.name}</h2>
+           
+
+        
+   `
+    
+     stadiumCollection.append(stadiumDiv)
+
 }
 
 
-function renderAllCards() {
-    fetch('http://localhost:3000/stadia')
-        .then(response => response.json())
-        .then(stadiaArr => {
-            stadiaArr.forEach(stadiaObject => {
+
+
+function renderAllCard () {
+
+    fetch ("http://localhost:3000/stadia")
+        .then (response => response.json())
+        .then (stadiaArr => {
+            stadiaArr.forEach (stadiaObject => {
                 renderOneCard(stadiaObject)
-            })
+            }) 
         })
+
+        
 }
+renderAllCard()
+
+stadiumCollection.addEventListener("click", (event) => {
+   
+    fetch (`http://localhost:3000/stadia/${event.target.dataset.id}`)
+    .then (response => response.json())
+    .then (renderOneCard) 
+    // <h4>${stadiumObj.description}</h4>
+
+})
+
+function stadiumInfo (stadiumInfoObj) {
+    
+
+}
+
+
+function renderOneUser (userObj) {
+   
+    const userDiv = document.createElement('div')
+    userDiv.classList.add('card')
+    userDiv.dataset.id = userObj.id
+
+    
+    userDiv.innerHTML = 
+       ` 
+            
+            <span> User </span>
+            
+      
+            <h2>${userObj.username}</h2>
+          
+        
+   `
+    
+     stadiumCollection.append(userDiv)
+
+}
+
+
+
+
+function renderAllUsers () {
+
+    fetch ("http://localhost:3000/users")
+        .then (response => response.json())
+        .then (userArr => {
+            userArr.forEach (userObject => {
+                renderOneUser(userObject)
+            }) 
+        })
+
+        
+}
+renderAllUsers()
